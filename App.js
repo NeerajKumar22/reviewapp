b170e882d1a6bcbe4ad93541d40c0901dcc3e500
NeerajKumar22 <ineerajkr@gmail.com>
2020-05-11 15:10:41 +0530

    applied drawer navigation in the application

--- App.js
+++ App.js
@@ -3,7 +3,8 @@ import * as Font from "expo-font";
 import { AppLoading } from "expo";
 
 import Home from "./screens/home";
-import Navigator from "./routes/homeStack";
+//import Navigator from "./routes/homeStack";
+import Navigator from "./routes/drawer";
 
 const getFonts = () => Font.loadAsync({
   'nunito-regular': require("./assets/fonts/Nunito-Regular.ttf"),
