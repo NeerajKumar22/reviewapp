3326fb90d51b4cffd08dc13d6b5de73a5099b80d
NeerajKumar22 <ineerajkr@gmail.com>
2020-05-15 23:11:18 +0530

    added some images for showing the rating

--- screens/reviewDetails.js
+++ screens/reviewDetails.js
@@ -1,22 +1,37 @@
 import React from "react";
-import { StyleSheet, Text, View, Button } from "react-native";
+import { StyleSheet, Text, View, Button, Image } from "react-native";
 
-import { globalStyles } from "../styles/global";
+import { globalStyles, images } from "../styles/global";
 import Card from "../shared/card";
 
 export default function ReviewDetails ({ navigation }) {
   const pressHandler = () => {
     navigation.goBack();
   }
+  const rating = navigation.getParam("rating");
   return (
     <View style={globalStyles.container}>
       <Card>
         {/*<Button title="back to home screen" onPress={pressHandler} /> */}
         <Text>{ navigation.getParam("title") }</Text>
         <Text>{ navigation.getParam("body") }</Text>
-        <Text>{ navigation.getParam("rating") }</Text>
+        <View style={styles.rating}>
+          <Text>ReviewApp Rating: </Text>
+          <Image source={images.ratings[rating]} />
+        </View>
       </Card>
     </View>
   )
 }
+
+const styles = StyleSheet.create({
+  rating: {
+    flexDirection: "row",
+    justifyContent: "center",
+    paddingTop: 16,
+    marginTop: 16,
+    borderTopWidth: 1,
+    borderTopColor: "#eee",
+  }
+})
 
